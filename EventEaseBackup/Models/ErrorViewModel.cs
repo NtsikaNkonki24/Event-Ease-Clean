44b5d80f23448c44225288a98c69505c359f44a4
NtsikaNkonki24 <NNkonki@singular.co.za>
2026-06-05 01:08:06 +0200

    Remove EventEaseBackup and exclude it from compilation

--- EventEaseBackup/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Event_Ease_2026_Ntsika_Nkonki.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
